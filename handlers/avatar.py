203394f6f1ac921988dbe9ad45796b3d01a0faa5
vincentpc <peter5547825@gmail.com>
2014-02-26 15:26:21 +0800

    Update avatar.py

--- handlers/avatar.py
+++ handlers/avatar.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import os
+import imghdr
+import mimetypes
+
+from webapp.web import BaseHandler
+from model import dbapi
+
+
+class AvatarHandler(BaseHandler):
+
+    def get(self, name):
+        fullpath = "images/" + name
+        filetype = "jpeg"
+        if os.path.isfile("images/" + name):
+            with open(fullpath, "rb") as f:
+                image = f.read()
+                length = len(image)
+                self.set_header("Content-Length", length)
+                filetype = imghdr.what(fullpath)
+            mtype = mimetypes.types_map.get("." + filetype, "image/jpeg")
+            self.set_header("Content-Type", mtype)
+            self.write(image)
+        else:
+            self.redirect("/error")
+
+    def post(self, name):
+        self.get(name)
+        
