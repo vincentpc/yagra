74a7c24b484915b6d3d578a9d96841c64125a0e2
vincentpc <peter5547825@gmail.com>
2014-02-26 15:26:52 +0800

    Update index.py

--- handlers/index.py
+++ handlers/index.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from webapp.web import BaseHandler
+from model import dbapi
+
+
+class IndexHandler(BaseHandler):
+
+    def get(self):
+
+        email = self.get_secure_cookie("email")
+        user = dbapi.User()
+        if email and user.get_user(email) == 0:
+            self.redirect("/user")
+        else:
+            self.clear_cookies()
+            body = self.wrap_html('templates/index.html')
+            self.write(body)
