070a90647e4fa7cec36d7da903af90bffbce04cb
vincentpc <peter5547825@gmail.com>
2014-02-26 15:19:36 +0800

    Update signin.py

--- handlers/signin.py
+++ handlers/signin.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from webapp.web import BaseHandler
+
+
+class SigninHandler(BaseHandler):
+
+    def post(self):
+        email = self.get_arg("email")
+        password = self.get_arg("password")
+        if email:
+            user = dbapi.User()
+            if user.check_user(email, password) == 0:
+                self.set_secure_cookie('email', str(email))
+                self.redirect("/user")
+
+        self.redirect("/")
