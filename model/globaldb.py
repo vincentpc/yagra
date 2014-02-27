887db0b77e55ae410d9af78a35e157cc27244f34
vincentpc <peter5547825@gmail.com>
2014-02-27 12:18:10 +0000

    db port added in config

--- model/globaldb.py
+++ model/globaldb.py
@@ -9,6 +9,7 @@ import config
 
 
 db = MySQLdb.connect(host=config.DB_HOST,
+                     port=config.DB_PORT,
                      user=config.DB_USER,
                      passwd=config.DB_PASSWD,
                      db=config.DB_NAME,
